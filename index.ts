762ba26934b397f46cb2ca62192c9d3a6b52bb5e
uttamkn <uttamkn15@gmail.com>
2024-09-02 17:21:25 +0530

    restart

--- index.ts
+++
@@ -1,8 +0,0 @@
-import app from "./app";
-/*
- * bun has a default server but still we will still use hono
- */
-
-Bun.serve({
-  fetch: app.fetch,
-});
--- routes/test.ts
+++
@@ -1,9 +0,0 @@
-import { Hono } from "hono";
-
-const test = new Hono();
-
-test.get("/", (c) => {
-  return c.json({ message: "hello world from test" });
-});
-
-export default test;
